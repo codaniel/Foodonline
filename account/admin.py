81d8620c0624b6da2795c25b97450f2c0a087acf
codaniel <140230536+codaniel@users.noreply.github.com>
2023-07-23 12:21:03 +0200

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display =('email', 'fist_name', 'last_name','username', 'role','is_active','is_staff' )
+    ordering = '-date_joined'
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets =()
+
+admin.register(User, CustomUserAdmin)
