3fa1f878dacf6bfc245808fb4e9df90a53e0fd17
codaniel <140230536+codaniel@users.noreply.github.com>
2023-07-25 21:55:10 +0200

    account update

--- account/views.py
+++ account/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserForm
 
 # Create your views here.
 def registerUser(request):
-    return HttpResponse('User Registration Page')
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('registerUser')
+    else:
+        form = UserForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'account/registerUser.html',context)
