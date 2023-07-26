2c6f0435f593ffd3fd7e63e664aac2ceab835147
codaniel <140230536+codaniel@users.noreply.github.com>
2023-07-26 21:29:04 +0200

    implemented password hashing and user registration validation

--- account/views.py
+++ account/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserForm
-
+from .models import User
 # Create your views here.
 def registerUser(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            #create the user using the form
+            #password = form.cleaned_data['password']
+            #user = form.save(commit=False)
+            #user.set_password(password)
+            #user.role = User.CUSTOMER
+            #user.save()
+
+            #Create the user using the createuser method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name = first_name, last_name = last_name, username = username, email = email, password = password)
+            user.role = User.CUSTOMER
+            user.save()
+
+        else:
+            print(form.errors)    
+
             return redirect('registerUser')
     else:
         form = UserForm()
